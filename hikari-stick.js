aa87d610574ea2abeaecd8855f96640134b4cfcc
梓漪 <ziazusa@foxmail.com>
2023-08-06 09:13:53 +0800

    Update hikari-stick.js

--- hikari-stick.js
+++ hikari-stick.js
@@ -1,3 +1,13 @@
+const changeColor = false;
+
+const randomStorage = {};
+
+const theRandom = id => {
+    if (!id) return Math.random();
+    if (!(id in randomStorage)) randomStorage[id] = Math.random();
+    return randomStorage[id];
+}
+
 // 创建观众对象 *param: rank 角色星级
 function createCustomers(rank) {
     removeCustomers();
@@ -9,8 +19,8 @@ function createCustomers(rank) {
         let name = "l" + String(i % 2);
         level.classList.add(name);
         let gap = -Number((400 / ln).toFixed(3)) * (i - 1.75);
-        level.style = `transform: translate3d(-50%, 0, ${gap}px); width: ${100 + 10 * i}%; bottom: ${30 * i}px; z-index: ${100 + (ln - i)};`;
-        const sn = Number(Math.ceil(main.offsetWidth / 80)) + Number(Math.floor(Math.random() * -3)) + 2 + (rank == 5 ? 5 : 0);
+        level.style = `transform: translate3d(-50%, 0, ${gap}px); width: ${100 + 10 * i}%; bottom: ${main.offsetHeight / ln * i}px; z-index: ${100 + (ln - i)};`;
+        const sn = Number(Math.ceil(main.offsetWidth / 80)) + Number(Math.floor(theRandom(`i${i}sn`) * -2)) + 3 + (rank == 5 ? 5 : 0);
         for (let j = 0; j < sn; j++) {
             const stick = template.querySelector(".stick").cloneNode(true);
             let color = (rank => {
@@ -21,16 +31,16 @@ function createCustomers(rank) {
                         return ["yellow"];
                     case 4:
                         let star4Color = ["white", "yellow", "red", "green", "blue", "aqua", "plum"];
-                        return [star4Color[Math.floor(Math.random() * star4Color.length)]];
+                        return [star4Color[Math.floor(theRandom((changeColor ? `i${i}j${j}s4c` : null)) * star4Color.length)]];
                     case 5:
                         let star5Color = ["white", "yellow", "red", "green", "blue", "plum"];
-                        return [star5Color[Math.floor(Math.random() * star5Color.length)], "star5"];
+                        return [star5Color[Math.floor(theRandom((changeColor ? `i${i}j${j}s5c` : null)) * star5Color.length)], "star5"];
                     default:
                         throw "Error!";
                 }
             })(rank);
             stick.querySelector(".stick-body").classList.add(...color);
-            stick.querySelector(".stick-body").style.left = String(Number(Math.floor(Math.random() * -21)) + 60) + "px";
+            stick.querySelector(".stick-body").style.left = String(Number(Math.floor(theRandom(`i${i}j${j}lp`) * -21)) + 60) + "px";
             level.appendChild(stick);
         }
         main.appendChild(level);
